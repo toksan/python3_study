b496b8dab18851be382500b34f0631cea7c81cda
toksan <t.moriya.ct@gmail.com>
2018-03-29 09:26:49 +0900

    ダウンロード例を追加

--- network/get_by_requests.py
+++ network/get_by_requests.py
@@ -12,3 +12,13 @@ try:
     print(r.text)
 except requests.exceptions.RequestException as err:
     print(err)
+
+
+# ダウンロード（読み込み + ローカル保存）
+# ダウンロードして rss.xml というファイル名で保存する例
+try:
+    r = requests.get(url, timeout=10.0)
+    with open('rss.xml', mode='w') as f:
+        f.write(r.text)
+except requests.exceptions.RequestException as err:
+    print(err)
